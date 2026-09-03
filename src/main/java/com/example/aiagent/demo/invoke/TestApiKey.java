8a124d3c2971f5be09e919b35b217aa59361174a
陶逸峰 <1016217161@qq.com>
2026-09-03 22:05:09 +0800

    2026/9/3：API-KEY优化

--- src/main/java/com/example/aiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/example/aiagent/demo/invoke/TestApiKey.java
@@ -6,5 +6,5 @@ package com.example.aiagent.demo.invoke;
 public interface TestApiKey {
 
     // 修改为你的 API Key
-    String API_KEY = "sk-4204fb2fc4f1423ba5ada4399f36448a";
+    String API_KEY = System.getenv("DASHSCOPE_API_KEY");
 }
